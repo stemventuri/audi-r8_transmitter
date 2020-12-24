f3ab3cf4a876e0e126b802d2e744e289444528e2
stemventuri <57156157+stemventuri@users.noreply.github.com>
2020-12-24 17:42:28 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendNumber(2)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendNumber(1)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendNumber(4)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    radio.sendNumber(3)
+})
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+	
+})
