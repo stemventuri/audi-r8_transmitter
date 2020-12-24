c568405ae46ba5860e9383df734005aa6215c090
stemventuri <57156157+stemventuri@users.noreply.github.com>
2020-12-24 21:46:19 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,46 @@
-Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Down, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendNumber(2)
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire1, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    basic.showString("F")
+    radio.sendValue("forward", 1)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
+    basic.showString("B")
+    radio.sendValue("backward", 1)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.clearScreen()
+    radio.sendValue("right", 0)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.clearScreen()
+    radio.sendValue("left", 0)
 })
 Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendNumber(1)
+    basic.showIcon(IconNames.No)
+    radio.sendValue("stop", 1)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire2, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.clearScreen()
+    radio.sendValue("backward", 0)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Up, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.clearScreen()
+    radio.sendValue("stop", 0)
+})
+Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Fire1, Kitronik_Game_Controller.ControllerButtonEvents.Up, function () {
+    basic.clearScreen()
+    radio.sendValue("forward", 0)
 })
 Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Right, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendNumber(4)
+    basic.showString("R")
+    radio.sendValue("right", 1)
 })
 Kitronik_Game_Controller.onButtonPress(Kitronik_Game_Controller.ControllerButtonPins.Left, Kitronik_Game_Controller.ControllerButtonEvents.Down, function () {
-    radio.sendNumber(3)
+    basic.showString("L")
+    radio.sendValue("left", 1)
 })
-radio.setGroup(1)
-basic.showIcon(IconNames.Yes)
+let radio_group = 1
+radio.setGroup(radio_group)
+basic.showNumber(radio_group)
 basic.forever(function () {
 	
 })
